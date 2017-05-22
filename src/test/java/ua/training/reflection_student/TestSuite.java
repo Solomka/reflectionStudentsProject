b314ea025de7c3b5058ac22ed57315eef40ff270
Solomka <sol.yaremko@gmail.com>
2017-05-22 06:58:06 +0300

    minor

--- src/test/java/ua/training/reflection_student/TestSuite.java
+++ src/test/java/ua/training/reflection_student/TestSuite.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import ua.training.reflection_student.model.proxy.GraduateStudentProxyHandler;
 import ua.training.reflection_student.service.ClassStatisticsServiceTest;
 
 @RunWith(Suite.class)
